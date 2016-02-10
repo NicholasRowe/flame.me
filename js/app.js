449ea5240411db77b90d2f6c9b52f6d13af9319d
nickrowe <nick.rowe@prodo.com>
2016-02-10 16:19:09 +0000

    init commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('myApp', [])
+  .controller('startScreen', function($scope, $http){
+    $scope.$watch('search', function() {
+      fetch();
+
+    });
+
+    $scope.$watch('searchSummoner', function() {
+      getSummoner();
+      
+    });
+
+   $scope.search = "Sherlock Holmes";
+    
+
+    
+
+    function fetch(){
+      $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+      .then(function(response){ 
+        $scope.details = response.data; 
+        // console.log(response.data);
+      });
+
+      $http.get("http://www.omdbapi.com/?s=" + $scope.search)
+      .then(function(response){ $scope.related = response.data; });
+    }
+
+    $scope.apiKey = "c7e84542-6264-4b87-b0a8-30bd8055dd83"
+    $scope.searchSummoner = "Test";
+
+    function getSummoner(){
+
+      $http.get("https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/" + $scope.searchSummoner + "?api_key=" + $scope.apiKey)
+      .then(function(response){ 
+        $scope.object = response.data; 
+        console.log(  $scope.object);
+
+      });
+
+    }
+
+    $scope.update = function(movie){
+      $scope.search = movie.Title;
+    };
+
+    $scope.select = function(){
+      this.setSelectionRange(0, this.value.length);
+    }
+  });
