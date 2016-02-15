d5dc268f66ecb75beee7c04816dc5844a541bcfb
nickrowe <nick.rowe@prodo.com>
2016-02-15 17:01:30 +0000

    first screen

--- js/app.js
+++ js/app.js
@@ -12,17 +12,8 @@ angular.module('myApp', [])
       
     });
 
-   $scope.search = "Sherlock Holmes";
-    
-
-    
 
     function fetch(){
-      $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
-      .then(function(response){ 
-        $scope.details = response.data; 
-        // console.log(response.data);
-      });
 
       $http.get("http://www.omdbapi.com/?s=" + $scope.search)
       .then(function(response){ $scope.related = response.data; });
@@ -46,7 +37,8 @@ angular.module('myApp', [])
       $scope.search = movie.Title;
     };
 
-    $scope.select = function(){
-      this.setSelectionRange(0, this.value.length);
-    }
+    // $scope.select = function(){
+    //   this.setSelectionRange(0, this.value.length);
+    // }
+    
   });
